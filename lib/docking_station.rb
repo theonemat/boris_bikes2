35e460b644edd1b7b1de7b96e80f35182bb27fc3
mat <matmoon14@gmail.com>
2019-04-02 15:31:23 +0100

    step 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,4 +1,8 @@
+require_relative 'bike'
+
+
 class DockingStation
-def release_bike
-end
+  def release_bike
+    Bike.new
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,13 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
   it 'responds to releas_bike' do
     expect(subject).to respond_to :release_bike
+  end
+
+  it 'creates new bike object if bike is working' do
+    bike = subject.release_bike
+    expect(bike).to be_working
   end
 end
